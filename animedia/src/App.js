ef2d8326f590b805e825e1d6c58f2abe1429148d
BUCKBROWN24 <brownbruce64@gmail.com>
2025-05-29 10:56:08 -0500

    Commited
    Frontend and Backend Integration

--- animedia/src/App.js
+++ animedia/src/App.js
@@ -2,7 +2,7 @@ import Nav from './Header/Header.js'
 import Main from './Main/Main.js'
 import Section from './Section/Section.js'
 import Footer from './Footer/Footer.js'
-
+import SearchAnime from './SearchAnime'
 
 // ⭐ I'm so sorry. I should have completed this 5 hours ago.  
 
@@ -12,6 +12,7 @@ function App() {
 
 <div> 
   <Nav/>
+  <SearchAnime />
   <Main/>
   <Section/>
   <Footer/>
